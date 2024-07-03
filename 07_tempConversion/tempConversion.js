e43ed46337b22ab809c7d108d18db69e69120c1e
kevintang6142 <kevintang6142@gmail.com>
2024-07-03 15:46:35 -0500

    Solve exercise 07_tempConversion/

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temperature) {
+  return +(5 / 9 * (temperature - 32)).toFixed(1);
+}
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return +(9 / 5 * temperature + 32).toFixed(1);
 };
 
 // Do not edit below this line
