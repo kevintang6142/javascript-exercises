374a0613255ee1bf8b55d335e4dca659938cc37e
kevintang6142 <kevintang6142@gmail.com>
2024-07-05 15:58:32 -0500

    Solve exercise 09_palindromes/

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = Array.from(string.toLowerCase()).filter(char => "abcdefghijklmnopqrstuvwxyz01234568789".includes(char))
+    return array.join("") === array.reverse().join("");
 };
 
 // Do not edit below this line
