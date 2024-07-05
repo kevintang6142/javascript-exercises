c103963fa68503ff75443c54f388e54b61437cb8
kevintang6142 <kevintang6142@gmail.com>
2024-07-05 16:24:03 -0500

    Solve exercise 12_findTheOldest/

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => getAge(oldest) < getAge(currentPerson) ? currentPerson : oldest);
 };
+
+const getAge = function(person) {
+    if (person.yearOfDeath === undefined) return new Date().getFullYear() - person.yearOfBirth;
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
